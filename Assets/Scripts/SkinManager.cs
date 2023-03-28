99d31c8f00a431ca20bbae4c2d26eb9b19c3f984
Shelby <snm19a@acu.edu>
2023-03-28 01:57:48 -0500

    Added ability to change character skin

---
+++ Assets/Scripts/SkinManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class SkinManager : MonoBehaviour
+{
+    public SpriteRenderer sr;
+    public List<Sprite> skins = new List<Sprite>();
+    private int selectedSkin = 0;
+    public GameObject playerskin;
+
+
+
+    public void NextOption()
+    {
+        selectedSkin = selectedSkin + 1;
+        if (selectedSkin == skins.Count)
+        {
+            selectedSkin = 0;
+        }
+        sr.sprite = skins[selectedSkin];
+    }
+    public void BackOption()
+    {
+        selectedSkin = selectedSkin - 1;
+        if (selectedSkin < skins.Count)
+        {
+            selectedSkin = 0;
+        }
+        sr.sprite = skins[selectedSkin];
+    }
+}
